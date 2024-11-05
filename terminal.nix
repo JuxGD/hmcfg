a54ae0e06e355fc103a8f28517b53c1084d04e51
JuxGD <jak@e.email>
2024-11-04 21:44:52 -0300

    im stupid

--- terminal.nix
+++ terminal.nix
@@ -1,5 +1,11 @@
 { config, pkgs, inputs, ... }:
 
+let
+  stable = inputs.stable.legacyPackages.${pkgs.system};
+  master = inputs.master.legacyPackages.${pkgs.system};
+  staging = inputs.staging.legacyPackages.${pkgs.system};
+  tetriofix = inputs.tetriofix.legacyPackages.${pkgs.system};
+in
 {
   programs = {
     thefuck.enable = true;
