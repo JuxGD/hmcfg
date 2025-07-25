b242809a9243ade2db30f0465bf44b1126d7b8a1
JuxGD <jak@e.email>
2025-07-25 02:23:43 -0400

    it looks like that's not how it's done but i'm sure this is

--- niri.nix
+++ niri.nix
@@ -4,23 +4,16 @@ let
   stable = inputs.stable.legacyPackages.${pkgs.system};
   master = inputs.master.legacyPackages.${pkgs.system};
   staging = inputs.staging.legacyPackages.${pkgs.system};
-
-  niri-dotfiles = pkgs.fetchFromGitHub {
-    owner = "ShibamRoy9826";
-    repo = "niri-dotfiles";
-    rev = "b941deb448c1aaea743cf709b6cd62a751fb1e37";
-    hash = "sha256-K3JeEJsk8ginvoPE9S2gzpyuBY0ydk+aSVh7WXMeCqU=";
-  }; # this is just a little test :3
 in
 {
-  home.file."~/Desktop/niri-dotfiles/".source = niri-dotfiles;
-
   home.activation = {
     applyNiriDotfiles = lib.hm.dag.entryAfter ["writeBoundary"] ''
       echo "Applying niri dotfiles..."
 
       SOURCE_DIR="$HOME/Desktop/niri-dotfiles"
       TARGET_DIR="$HOME/.config"
+
+      git clone https://github.com/ShibamRoy9826/niri-dotfiles $SOURCE_DIR
 
       rm -r "$SOURCE_DIR/screenshots"
       rm "$SOURCE_DIR/README.md"
