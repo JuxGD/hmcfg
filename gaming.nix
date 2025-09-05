dda0131a9a5a5b4aec9b32547696cce57553a49b
Jux <jak@e.email>
2025-09-05 17:45:10 -0400

    please PLEEEEASE pleaseeeeee

--- gaming.nix
+++ gaming.nix
@@ -6,6 +6,19 @@ let
   staging = inputs.staging.legacyPackages.${pkgs.system};
 in
 {
+  nixpkgs.overlays = [
+    (final: prev: {
+      prismlauncher-unwrapped = prev.prismlauncher-unwrapped.overrideAttrs (old: {
+        patches = (old.patches or []) ++ [
+          (prev.fetchpatch {
+            url = "file:///home/j/its-a/secret.patch";
+	    hash = "07sx98d422589gxr8wflfpkdd0k44kbagxl3b51i56ky2wfix7rc";
+	  };
+	];
+      });
+    })
+  ];
+
   home.packages = with pkgs; [
     prismlauncher
     pcsx2
