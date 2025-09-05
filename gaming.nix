8012d695c3800db4d20bd910a2e5c143d4688a0f
Jux <jak@e.email>
2025-09-04 22:53:11 -0400

    add super secret personal fork of prismlauncher (it's a secret)

--- gaming.nix
+++ gaming.nix
@@ -6,8 +6,22 @@ let
   staging = inputs.staging.legacyPackages.${pkgs.system};
 in
 {
+  nixpkgs.overlays = [
+    (final: prev: {
+      prismlauncher-supersecretpersonalfork = prev.prismlauncher.override {
+        src = pkgs.fetchgit {
+          url = "file:///home/j/GitRepos/PrismLauncher";
+	  rev = "0e6517f2074e5a33f22f972e50d7324284ef9bef";
+	  fetchSubmodules = true;
+	  deepClone = true;
+	  hash = "bafddfaa18a3c345c3135c1455eb209c4b9142737e396a65588c4126153ca5ee";
+	};
+      };
+    })
+  ];
+
   home.packages = with pkgs; [
-    prismlauncher
+    prismlauncher-supersecretpersonalfork
     pcsx2
     rpcs3
     ryubing
