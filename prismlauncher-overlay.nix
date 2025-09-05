d0fdb45d4ac8bff4780052a662f8bd577e2e562f
Jux <jak@e.email>
2025-09-04 23:27:51 -0400

    oh i'm dumb there was a problem in the prismlauncher thing i fixed it and updated the rev and hash here

--- prismlauncher-overlay.nix
+++ prismlauncher-overlay.nix
@@ -2,10 +2,10 @@ final: prev: {
   prismlauncher-unwrapped = prev.prismlauncher-unwrapped.override {
     src = pkgs.fetchgit {
       url = "file:///home/j/GitRepos/PrismLauncher";
-      rev = "0e6517f2074e5a33f22f972e50d7324284ef9bef";
+      rev = "70465067cb6bf192675bbda47eb88b175c2a7174";
       fetchSubmodules = true;
       deepClone = true;
-      hash = "bafddfaa18a3c345c3135c1455eb209c4b9142737e396a65588c4126153ca5ee";
+      hash = "39b15ab6f0dabb608ca591ceff2494e8d6e53f2564ded3f4812e8914c934847e";
     };
   };
 }
