c3d6e4e27ea9b30d402cf0701f91cb613d06eee5
Jux <jak@e.email>
2025-09-04 23:42:41 -0400

    wtf does old mean

--- prismlauncher-overlay.nix
+++ prismlauncher-overlay.nix
@@ -1,7 +1,7 @@
 { pkgs, lib, config, inputs, ... }: {
   nixpkgs.overlays = [
     (final: prev: {
-      prismlauncher-unwrapped = prev.prismlauncher-unwrapped.override {
+      prismlauncher-unwrapped = prev.prismlauncher-unwrapped.overrideAttrs (old: {
         src = builtins.fetchgit {
           url = "file:///home/j/GitRepos/PrismLauncher";
           rev = "70465067cb6bf192675bbda47eb88b175c2a7174";
@@ -9,7 +9,7 @@
           deepClone = true;
           hash = "39b15ab6f0dabb608ca591ceff2494e8d6e53f2564ded3f4812e8914c934847e";
         };
-      };
+      });
     })
   ];
 }
