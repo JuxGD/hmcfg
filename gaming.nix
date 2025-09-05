f036e11973c615273807da1cc75063e237767444
Jux <jak@e.email>
2025-09-05 16:54:28 -0400

    orrr like this

--- gaming.nix
+++ gaming.nix
@@ -8,14 +8,14 @@ in
 {
   nixpkgs.overlays = [
     (final: prev: {
-      prismlauncher-unwrapped = prev.prismlauncher-unwrapped.override {
+      prismlauncher-unwrapped = prev.prismlauncher-unwrapped.overrideAttrs (finalAttrs: previousAttrs: {
         src = pkgs.fetchgit {
           url = "file:///home/j/GitRepos/PrismLauncher";
 	  rev = "0e6517f2074e5a33f22f972e50d7324284ef9bef";
 	  fetchSubmodules = true;
 	  deepClone = true;
 	  hash = "bafddfaa18a3c345c3135c1455eb209c4b9142737e396a65588c4126153ca5ee";
-	};
+	});
       };
     })
   ];
