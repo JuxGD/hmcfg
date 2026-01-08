253aefcaa8c143584ae3939a17703ba1ccce47c9
Jux <jak@e.email>
2026-01-08 17:51:59 -0300

    wha

--- gaming.nix
+++ gaming.nix
@@ -6,20 +6,6 @@ let
   staging = inputs.staging.legacyPackages.${pkgs.system};
 in
 {
-  nixpkgs.overlays = [
-    inputs.nur.overlays
-    (final: prev: {
-      prismlauncher-unwrapped = prev.prismlauncher-unwrapped.overrideAttrs (old: {
-        patches = (old.patches or []) ++ [
-          (prev.fetchpatch {
-            url = "http://localhost:3000/JuxGD/its-a/raw/branch/main/secret.patch";
-	    hash = "sha256-iExaBw/fAorz3vj7HJX8dV3bDlcJdFVF44s+T5whnY4=";
-	  })
-	];
-      });
-    })
-  ];
-
   home.packages = with pkgs; [
     prismlauncher
     pcsx2
