f6c15047f73057f5c4cf732db2ab4fb5c0f3bdef
Jux <jak@e.email>
2026-06-06 14:50:15 -0400

    what

--- flake.nix
+++ flake.nix
@@ -15,8 +15,12 @@
 
     nix-flatpak.url = "github:gmodena/nix-flatpak";
 
+    niri-package.url = "github:urayde/niri";
+
     niri = {
       url = "github:sodiboo/niri-flake";
+      inputs.niri-unstable.follows = "niri-package";
+      inputs.nixpkgs.follows = "nigxkgs";
     };
 
     nur = {
